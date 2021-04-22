af9a3a9063fb760509fc007bf60bf5a2a325992b
JeonMinJae <ssi02014@naver.com>
2021-04-22 20:58:38 +0900

    Add(minjae/authPage): login redux 제거하고 axios로 변경 #4

--- app/redux/sagas/authSaga.js
+++ app/redux/sagas/authSaga.js
@@ -1,39 +1,10 @@
 import axios from "axios";
 import { all, fork, put, takeEvery, call } from "redux-saga/effects";
 import {
-  LOGIN_FAILURE,
-  LOGIN_REQUEST,
-  LOGIN_SUCCESS,
   LOGIN_CHECK_FAILURE,
   LOGIN_CHECK_SUCCESS,
   LOGIN_CHECK_REQUEST,
 } from "../types";
-
-// LOGIN
-function loginUserAPI(loginData) {
-  return axios.post(`${process.env.API_URL}/api/jwt`, loginData);
-}
-
-function* loginUser(action) {
-  try {
-    const result = yield call(loginUserAPI, action.payload);
-
-    yield put({
-      type: LOGIN_SUCCESS,
-      payload: result.data,
-    });
-
-    yield put({
-      type: LOGIN_CHECK_REQUEST,
-      payload: localStorage.getItem("jwt"),
-    });
-  } catch (e) {
-    yield put({
-      type: LOGIN_FAILURE,
-      payload: e.response,
-    });
-  }
-}
 
 // Loading
 function loginCheckAPI(token) {
@@ -73,15 +44,11 @@ function* loginCheck(action) {
   }
 }
 
-function* watchLoginUser() {
-  yield takeEvery(LOGIN_REQUEST, loginUser);
-}
-
 function* watchLoginCheck() {
   yield takeEvery(LOGIN_CHECK_REQUEST, loginCheck);
 }
 
 //authSaga() 여러 Saga 통합
 export default function* authSaga() {
-  yield all([fork(watchLoginUser), fork(watchLoginCheck)]);
+  yield all([fork(watchLoginCheck)]);
 }
