8dae7b5d1908608cfa34d14c6dcdbc2e042bdb37
JeonMinJae <ssi02014@naver.com>
2021-04-24 00:20:43 +0900

    Add(minjae/boards): boardListItem #6

---
+++ app/pages/boards/[categoryName]/search.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const Search = () => {
+  const router = useRouter();
+  console.log(router);
+
+  return (
+    <div>
+      <h1>search</h1>
+      <h1>search</h1>
+      <h1>search</h1>
+      <h1>search</h1>
+      <h1>search</h1>
+    </div>
+  );
+};
+
+export default Search;
