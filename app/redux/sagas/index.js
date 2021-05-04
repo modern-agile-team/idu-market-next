de4f2d15c9f177d351de8c828727a433851672cf
JeonMinJae <ssi02014@naver.com>
2021-05-04 13:27:03 +0900

    Add(minjae/boardDetail): comment Redux Setting #13

--- app/redux/sagas/index.js
+++ app/redux/sagas/index.js
@@ -1,8 +1,9 @@
 import { all, fork } from "redux-saga/effects";
 import authSaga from "./authSaga";
 import boardSaga from "./boardSaga";
+import commentSaga from "./commentSaga";
 
 //제너레이터
 export default function* rootSaga() {
-  yield all([fork(authSaga), fork(boardSaga)]);
+  yield all([fork(authSaga), fork(boardSaga), fork(commentSaga)]);
 }
