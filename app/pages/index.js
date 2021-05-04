d01a4880dd0e0d63c702285e13003ad80ee4f613
JeonMinJae <ssi02014@naver.com>
2021-05-04 18:32:05 +0900

    Add(minjae/boardDetail): Head Title 추가 #13

--- app/pages/index.js
+++ app/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>IUAM | 홈 화면</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainBanner></MainBanner>
