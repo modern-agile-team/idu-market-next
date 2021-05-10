0202bfe37e905d71ccbdabcde5faef07f53df0fd
JeonMinJae <ssi02014@naver.com>
2021-05-10 15:37:53 +0900

    Add(minjae/tradeComplete): Create Routing complete #26

---
+++ app/pages/boards/[categoryName]/[num]/[studentId]/complete.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const complete = () => {
+  return (
+    <div>
+      <h1>hihi</h1>
+      <h1>hihi</h1>
+      <h1>hihi</h1>
+      <h1>hihi</h1>
+      <h1>hihi</h1>
+      <h1>hihi</h1>
+      <h1>hihi</h1>
+      <h1>hihi</h1>
+      <h1>hihi</h1>
+    </div>
+  );
+};
+
+export default complete;
