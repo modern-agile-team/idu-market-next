543b4dce5595627facf3cc78fade1a23311f170d
JeonMinJae <ssi02014@naver.com>
2021-05-10 15:34:54 +0900

    Add(minjae/tradeComplete): trade complete Saga #26

--- app/redux/reducers/index.js
+++ app/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import auth from "./authReducer";
 import board from "./boardReducer";
 import comment from "./commentReducer";
+import trade from "./tradeReducer";
 
 export default combineReducers({
   auth: auth,
   board: board,
   comment: comment,
+  trade: trade,
 });
