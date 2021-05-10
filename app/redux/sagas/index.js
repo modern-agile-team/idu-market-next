33a4003adf7bc775bed894bf9aecdb77ccd5c9ad
JeonMinJae <ssi02014@naver.com>
2021-05-10 15:34:54 +0900

    Add(minjae/tradeComplete): trade complete Saga #26

--- app/redux/sagas/index.js
+++ app/redux/sagas/index.js
@@ -2,8 +2,14 @@ import { all, fork } from "redux-saga/effects";
 import authSaga from "./authSaga";
 import boardSaga from "./boardSaga";
 import commentSaga from "./commentSaga";
+import tradeSaga from "./tradeSaga";
 
 //제너레이터
 export default function* rootSaga() {
-  yield all([fork(authSaga), fork(boardSaga), fork(commentSaga)]);
+  yield all([
+    fork(authSaga),
+    fork(boardSaga),
+    fork(commentSaga),
+    fork(tradeSaga),
+  ]);
 }
---
+++ app/redux/sagas/tradeSaga.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+import { all, fork, put, takeEvery, call } from "redux-saga/effects";
+import {
+  TRADE_COMMENT_GET_REQUEST,
+  TRADE_COMMENT_GET_SUCCESS,
+  TRADE_COMMENT_GET_FAILURE,
+  TRADE_COMPLETE_REQUEST,
+  TRADE_COMPLETE_SUCCESS,
+  TRADE_COMPLETE_FAILURE,
+} from "../types";
+
+//Trade Comment Get
+function tradeCommentGetAPI(payload) {
+  const categoryName = payload.categoryName;
+  const num = payload.num;
+
+  return axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/boards/${categoryName}/${num}/comments`
+  );
+}
+
+function* tradeCommentGet(action) {
+  try {
+    const result = yield call(tradeCommentGetAPI, action.payload);
+
+    yield put({
+      type: TRADE_COMMENT_GET_SUCCESS,
+      payload: result.data,
+    });
+  } catch (e) {
+    yield put({
+      type: TRADE_COMMENT_GET_FAILURE,
+      payload: e.response,
+    });
+  }
+}
+
+//Trade Complete
+function tradeCompleteAPI(payload) {
+  const boardNum = payload.boardNum;
+  const nickname = payload.nickname;
+
+  const body = {
+    boardNum,
+    nickname,
+  };
+
+  return axios.post(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/purchase-list`,
+    body
+  );
+}
+
+function* tradeComplete(action) {
+  try {
+    const result = yield call(tradeCompleteAPI, action.payload);
+
+    yield put({
+      type: TRADE_COMPLETE_SUCCESS,
+      payload: result.data,
+    });
+  } catch (e) {
+    yield put({
+      type: TRADE_COMPLETE_FAILURE,
+      payload: e.response,
+    });
+  }
+}
+
+function* watchTradeCommentGet() {
+  yield takeEvery(TRADE_COMMENT_GET_REQUEST, tradeCommentGet);
+}
+
+function* watchTradeComplete() {
+  yield takeEvery(TRADE_COMPLETE_REQUEST, tradeComplete);
+}
+
+export default function* tradeSaga() {
+  yield all([fork(watchTradeCommentGet), fork(watchTradeComplete)]);
+}
