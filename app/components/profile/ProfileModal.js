c15aade453046a61a6a16141a15b5e5b22f1d28d
JeonMinJae <ssi02014@naver.com>
2021-05-11 15:30:30 +0900

    Add(minjae/profile): profile Image modal #29

--- app/components/profile/ProfileModal.js
+++ app/components/profile/ProfileModal.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { MdEmail, MdCancel } from "react-icons/md";
 
-const ProfileModal = () => {
-  return <div></div>;
+const ProfileModal = ({ onSelectImage, onModalCancel }) => {
+  return (
+    <div className="profile-img-modal">
+      <div className="img-select-box" onClick={onSelectImage}>
+        <img
+          src="https://wooahan-agile.s3.ap-northeast-2.amazonaws.com/profile/1.png"
+          alt="프로필 이미지 선택"
+        />
+      </div>
+      <div className="img-select-box" onClick={onSelectImage}>
+        <img
+          src="https://wooahan-agile.s3.ap-northeast-2.amazonaws.com/profile/2.png"
+          alt="프로필 이미지 선택"
+        />
+      </div>
+      <div className="img-select-box" onClick={onSelectImage}>
+        <img
+          src="https://wooahan-agile.s3.ap-northeast-2.amazonaws.com/profile/3.png"
+          alt="프로필 이미지 선택"
+        />
+      </div>
+      <div className="img-select-box" onClick={onSelectImage}>
+        <img
+          src="https://wooahan-agile.s3.ap-northeast-2.amazonaws.com/profile/4.png"
+          alt="프로필 이미지 선택"
+        />
+      </div>
+      <div className="img-select-box" onClick={onSelectImage}>
+        <img
+          src="https://wooahan-agile.s3.ap-northeast-2.amazonaws.com/profile/5.png"
+          alt="프로필 이미지 선택"
+        />
+      </div>
+      <div className="img-select-box" onClick={onSelectImage}>
+        <img
+          src="https://wooahan-agile.s3.ap-northeast-2.amazonaws.com/profile/6.png"
+          alt="프로필 이미지 선택"
+        />
+      </div>
+      <div className="img-modal-top">
+        <p className="img-modal-title">
+          IUAM
+          <span>Select Profile Image</span>
+        </p>
+        <MdCancel onClick={onModalCancel} className="img-modal-cancel" />
+      </div>
+    </div>
+  );
 };
 
 export default ProfileModal;
