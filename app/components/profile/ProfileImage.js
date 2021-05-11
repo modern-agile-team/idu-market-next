7dfe1eea0af61207199cd00714c37471633286c4
JeonMinJae <ssi02014@naver.com>
2021-05-11 15:30:30 +0900

    Add(minjae/profile): profile Image modal #29

--- app/components/profile/ProfileImage.js
+++ app/components/profile/ProfileImage.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BiPencil } from "react-icons/bi";
 import { PROFILE_IMAGE_UPDATE_REQUEST } from "../../redux/types";
+import ProfileModal from "./ProfileModal";
 
 const ProfileImage = ({ profileDetail, studentId }) => {
   const [openImgSelectModal, setOpenImgSelectModal] = useState(false);
@@ -10,15 +11,15 @@ const ProfileImage = ({ profileDetail, studentId }) => {
   const { id } = useSelector((state) => state.auth);
 
   const onSelectImage = (e) => {
-    // const body = {
-    //   studentId,
-    //   profilePath: e.target.src,
-    // };
+    const body = {
+      studentId,
+      profilePath: e.target.src,
+    };
 
-    // dispatch({
-    //   type: PROFILE_IMAGE_UPDATE_REQUEST,
-    //   payload: body,
-    // });
+    dispatch({
+      type: PROFILE_IMAGE_UPDATE_REQUEST,
+      payload: body,
+    });
 
     setOpenImgSelectModal(false);
   };
@@ -31,6 +32,15 @@ const ProfileImage = ({ profileDetail, studentId }) => {
         <BiPencil
           className="profile-img-update"
           onClick={() => setOpenImgSelectModal(true)}
+        />
+      ) : (
+        <></>
+      )}
+
+      {openImgSelectModal ? (
+        <ProfileModal
+          onSelectImage={onSelectImage}
+          onModalCancel={() => setOpenImgSelectModal(false)}
         />
       ) : (
         <></>
