04e1a82225a4e0896eead4b531c6a443cfc14d0f
JeonMinJae <ssi02014@naver.com>
2021-05-11 15:23:41 +0900

    Add(minjae/profile): profile Image UI #29

---
+++ app/components/profile/ProfileImage.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BiPencil } from "react-icons/bi";
+import { PROFILE_IMAGE_UPDATE_REQUEST } from "../../redux/types";
+
+const ProfileImage = ({ profileDetail, studentId }) => {
+  const [openImgSelectModal, setOpenImgSelectModal] = useState(false);
+
+  const dispatch = useDispatch();
+  const { id } = useSelector((state) => state.auth);
+
+  const onSelectImage = (e) => {
+    // const body = {
+    //   studentId,
+    //   profilePath: e.target.src,
+    // };
+
+    // dispatch({
+    //   type: PROFILE_IMAGE_UPDATE_REQUEST,
+    //   payload: body,
+    // });
+
+    setOpenImgSelectModal(false);
+  };
+
+  return (
+    <div className="profile-img-box">
+      <img src={profileDetail.profilePath} alt="test" className="profile-img" />
+
+      {id === studentId ? (
+        <BiPencil
+          className="profile-img-update"
+          onClick={() => setOpenImgSelectModal(true)}
+        />
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+export default ProfileImage;
--- app/pages/students/[studentId].js
+++ app/pages/students/[studentId].js
@@ -1,19 +1,37 @@
 import React, { useState, useEffect } from "react";
 import BoardBanner from "../../components/Board/BoardBanner";
 import { useDispatch, useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import { PROFILE_GET_REQUEST } from "../../redux/types";
+import ProfileImage from "../../components/profile/ProfileImage";
 
 const Profile = () => {
+  const router = useRouter();
+  const { studentId } = router.query;
+  const dispatch = useDispatch();
+  const profileDetail = useSelector((state) => state.profile.profile);
+
+  useEffect(() => {
+    dispatch({
+      type: PROFILE_GET_REQUEST,
+      payload: studentId,
+    });
+  }, [studentId]);
+
   return (
     <>
+      <Head>
+        <title>IUAM | 프로필</title>
+      </Head>
       <BoardBanner title="IUAM" desc="profile" />
       <section className="profile" id="profile">
         <div className="container">
-          <h1>hi</h1>
-          <h1>hi</h1>
-          <h1>hi</h1>
-          <h1>hi</h1>
-          <h1>hi</h1>
-          <h1>hi</h1>
+          {profileDetail ? (
+            <ProfileImage profileDetail={profileDetail} studentId={studentId} />
+          ) : (
+            <></>
+          )}
         </div>
       </section>
     </>
