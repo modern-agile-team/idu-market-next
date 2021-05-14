7db1982482e7ac15e1a2a00595761015d6b86745
JeonMinJae <ssi02014@naver.com>
2021-05-14 15:27:18 +0900

    Style(minjae/watchlist): foler Name Modify #28

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -5,7 +5,7 @@ import { createWrapper } from "next-redux-wrapper";
 import store from "../redux/store";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import loginCheck from "../components/auth/loginCheck";
+import loginCheck from "../components/Auth/loginCheck";
 
 import "../scss/main.scss";
 import "react-quill/dist/quill.snow.css";
--- app/pages/watchlist/[studentId].js
+++ app/pages/watchlist/[studentId].js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
 import BoardBanner from "../../components/Board/BoardBanner";
 import Head from "next/head";
-import WatchlistComponent from "../../components/Watchlist/WatchlistComponent";
+import MarketListItem from "../../components/Board/MarketListItem";
 
 const Watchlist = () => {
+  const [productList, setProductList] = useState([]);
+
+  const router = useRouter();
+  const { studentId } = router.query;
+
+  const { id } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (id.length > 0) {
+      if (studentId !== id) {
+        alert("잘못된 접근입니다.");
+        router.push("/");
+      } else {
+        axios
+          .get(`${process.env.NEXT_PUBLIC_API_URL}/api/watchlist/${studentId}`)
+          .then((response) => {
+            if (response.data.success) {
+              const result = response.data.boards;
+              setProductList(result);
+            }
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      }
+    }
+  }, [studentId, id]);
+
   return (
     <>
       <Head>
@@ -11,8 +42,11 @@ const Watchlist = () => {
       </Head>
       <BoardBanner title="Profile" desc="watchlist" />
       <section className="market" id="market">
+        <h1 className="watchlist-title">
+          {`관심 목록 (${productList.length})`}
+        </h1>
         <div className="container">
-          <WatchlistComponent />
+          <MarketListItem productList={productList} profile />
         </div>
       </section>
     </>
