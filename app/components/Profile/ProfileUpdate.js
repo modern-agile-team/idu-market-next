3403303207598304d14204f8430fc85d3527cd4c
JeonMinJae <ssi02014@naver.com>
2021-05-17 16:39:54 +0900

    Add(minjae/profileUpdate): 프로필 수정 유효성 검사 #30

--- app/components/Profile/ProfileUpdate.js
+++ app/components/Profile/ProfileUpdate.js
@@ -21,14 +21,14 @@ const ProfileUpdate = ({ formValues, profileList, studentId }) => {
 
   const onChange = (e) => {
     setChangeFormValues({
-      ...formValues,
+      ...changeFormValues,
       [e.target.name]: e.target.value,
     });
   };
 
   const onChangeMajor = (e) => {
     setChangeFormValues({
-      ...formValues,
+      ...changeFormValues,
       majorNum: e.target.value,
     });
   };
@@ -39,13 +39,33 @@ const ProfileUpdate = ({ formValues, profileList, studentId }) => {
     const { email, nickname, majorNum } = changeFormValues;
 
     let body = {
-      studentId: studentId,
+      studentId,
       email,
       nickname,
       majorNum,
     };
 
-    console.log(body);
+    if ([nickname, email].includes("")) {
+      setErrorMsg("빈 칸을 모두 입력하세요.");
+    } else if (nickname.match(/^[a-zA-Z가-힣0-9]{2,10}$/) === null) {
+      setErrorMsg("별명은 2 ~ 10자리입니다. 모음, 자음 따로입력 불가");
+    } else if (
+      email !== "" &&
+      email.match(
+        /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+      ) === null
+    ) {
+      setErrorMsg("이메일 형식을 유지해주세요.");
+    } else if (majorNum.length === 0) {
+      setErrorMsg("학과를 선택해주세요.");
+    } else {
+      setErrorMsg("");
+      // dispatch({
+      //   type:
+      //   payload: body,
+      // });
+      console.log(body);
+    }
   };
 
   return (
