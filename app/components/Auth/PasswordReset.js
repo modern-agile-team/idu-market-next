8a13eeb4ab60d0b1290d9541bf8bf2e07dd68f27
JeonMinJae <ssi02014@naver.com>
2021-05-20 16:03:40 +0900

    Add(minjae/pwdChange): password Change 기능 구현 #47

---
+++ app/components/Auth/PasswordReset.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+const PasswordReset = ({ formValues, errorMsg, onSubmit, onChange }) => {
+  return (
+    <>
+      <form className="form-field">
+        <h1 className="form-title">Reset Password</h1>
+        <div className="text-field">
+          <input
+            type="text"
+            name="id"
+            onChange={onChange}
+            className="input-text"
+            autoComplete="off"
+          />
+          <span
+            className={formValues.id ? "input-border fill" : "input-border"}
+          />
+          <label className={formValues.id ? "input-label fix" : "input-label"}>
+            학번
+          </label>
+        </div>
+        <div className="text-field">
+          <input
+            type="password"
+            name="psword"
+            onChange={onChange}
+            className="input-text"
+            autoComplete="off"
+          />
+          <span
+            className={formValues.psword ? "input-border fill" : "input-border"}
+          />
+          <label
+            className={formValues.psword ? "input-label fix" : "input-label"}
+          >
+            비밀번호{" "}
+            <label className="psword-text">
+              (영대소문자, 숫자, 특수문자 포함 9-20자)
+            </label>
+          </label>
+        </div>
+
+        <div className="text-field">
+          <input
+            type="password"
+            name="pswordConfirm"
+            onChange={onChange}
+            className="input-text"
+            autoComplete="off"
+          />
+          <span
+            className={
+              formValues.pswordConfirm ? "input-border fill" : "input-border"
+            }
+          />
+          <label
+            className={
+              formValues.pswordConfirm ? "input-label fix" : "input-label"
+            }
+          >
+            비밀번호 확인
+          </label>
+        </div>
+
+        <p className="form-errmsg">{errorMsg}</p>
+
+        <input
+          type="submit"
+          value="Reset Password"
+          onClick={onSubmit}
+          className="form-submit"
+        />
+      </form>
+    </>
+  );
+};
+
+export default PasswordReset;
--- app/pages/reset/password/[token].js
+++ app/pages/reset/password/[token].js
@@ -1,14 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+
+import PasswordReset from "../../../components/Auth/PasswordReset";
 import BoardBanner from "../../../components/Board/BoardBanner";
 
 const PasswordResetPage = () => {
+  const [errorMsg, setErrorMsg] = useState("");
+  const [formValues, setFormValues] = useState({
+    id: "",
+    psword: "",
+    pswordConfirm: "",
+  });
+
+  const router = useRouter();
+  const { jwt } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (jwt) router.push("/");
+  }, [jwt]);
+
+  const onChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const { id, psword, pswordConfirm } = formValues;
+
+    if ([id, psword, pswordConfirm].includes("")) {
+      setErrorMsg("빈 칸을 모두 입력하세요.");
+    } else if (
+      psword.match(
+        /^.*(?=^.{9,20}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[~!@#$%^&+=]).*$/
+      ) === null
+    ) {
+      setErrorMsg(
+        "비밀번호가 양식(영대소문자, 숫자, 특수문자 조합 9-20자)에 벗어났습니다."
+      );
+    } else if (psword !== pswordConfirm) {
+      setErrorMsg("비밀번호가 일치하지 않습니다.");
+    } else {
+      const body = {
+        id,
+        newPsword: psword,
+      };
+
+      console.log(body);
+      axios
+        .patch(`${process.env.NEXT_PUBLIC_API_URL}/api/password`, body)
+        .then((response) => {
+          if (response.data.success) {
+            console.log(response.data);
+            alert(`비밀번호 변경이 완료되었습니다.`);
+            router.push("/login");
+          }
+        })
+        .catch((err) => {
+          const response = err.response;
+          if (response.status === 409) {
+            setErrorMsg(response.data.msg);
+          }
+        });
+    }
+  };
+
   return (
     <>
       <BoardBanner title="IUAM" desc="reset" />
-      <h1>hi</h1>
-      <h1>hi</h1>
-      <h1>hi</h1>
-      <h1>hi</h1>
+      <section id="form-template" className="form-template">
+        <div className="container">
+          <PasswordReset
+            formValues={formValues}
+            errorMsg={errorMsg}
+            onSubmit={onSubmit}
+            onChange={onChange}
+          />
+        </div>
+      </section>
     </>
   );
 };
