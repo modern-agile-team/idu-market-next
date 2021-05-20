4526fc93d61993b376105a5d3e7de3c5ca614d24
JeonMinJae <ssi02014@naver.com>
2021-05-20 12:49:34 +0900

    Add(minjae/pwdChange): pwdChange Page js파일 생성 #47

---
+++ app/pages/reset/password/[token].js
@@ -0,0 +1,16 @@
+import React from "react";
+import BoardBanner from "../../../components/Board/BoardBanner";
+
+const PasswordResetPage = () => {
+  return (
+    <>
+      <BoardBanner title="IUAM" desc="reset" />
+      <h1>hi</h1>
+      <h1>hi</h1>
+      <h1>hi</h1>
+      <h1>hi</h1>
+    </>
+  );
+};
+
+export default PasswordResetPage;
