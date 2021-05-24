0119736e7221968043ea9b21dc6deebe0fe1997f
JeonMinJae <ssi02014@naver.com>
2021-05-24 13:45:04 +0900

    Add(minjae/notification): notification UI #49

--- app/components/Notification/Notification.js
+++ app/components/Notification/Notification.js
@@ -1,37 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-import { IoNotificationsCircleOutline } from "react-icons/io";
+import { IoMdNotifications } from "react-icons/io";
 
 const Notification = () => {
+  const [notification, setNotification] = useState(false);
+
   return (
     <div className="header-notification">
-      <ul>
-        <IoNotificationsCircleOutline />
-        <Link href="/">
-          <li>알림이 도착했습니다.</li>
-        </Link>
-        <Link href="/">
-          <li>알림이 도착했습니다.</li>
-        </Link>
-        <Link href="/">
-          <li>알림이 도착했습니다.</li>
-        </Link>
-        <Link href="/">
-          <li>알림이 도착했습니다.</li>
-        </Link>
-        <Link href="/">
-          <li>알림이 도착했습니다.</li>
-        </Link>
-        <Link href="/">
-          <li>알림이 도착했습니다.</li>
-        </Link>
-        <Link href="/">
-          <li>알림이 도착했습니다.</li>
-        </Link>
-        <Link href="/">
-          <li>알림이 도착했습니다.</li>
-        </Link>
-      </ul>
+      <IoMdNotifications
+        className={
+          notification ? "notification-icon focus" : "notification-icon"
+        }
+        onClick={() => setNotification(!notification)}
+      />
+
+      {notification ? (
+        <ul className="notification-ul">
+          <li className="notification-li nowatch">
+            <Link href="/">
+              <a>알림이 도착했습니다.</a>
+            </Link>
+          </li>
+          <li className="notification-li">
+            <Link href="/">
+              <a>알림이 도착했습니다.</a>
+            </Link>
+          </li>
+          <li className="notification-li">
+            <Link href="/">
+              <a>알림이 도착했습니다.</a>
+            </Link>
+          </li>
+          <li className="notification-li">
+            <Link href="/">
+              <a>알림이 도착했습니다.</a>
+            </Link>
+          </li>
+          <li className="notification-li">
+            <Link href="/">
+              <a>알림이 도착했습니다.</a>
+            </Link>
+          </li>
+          <li className="notification-li">
+            <Link href="/">
+              <a>알림이 도착했습니다.</a>
+            </Link>
+          </li>
+          <li className="notification-li">
+            <Link href="/">
+              <a>알림이 도착했습니다.</a>
+            </Link>
+          </li>
+          <li className="notification-li">
+            <Link href="/">
+              <a>알림이 도착했습니다.</a>
+            </Link>
+          </li>
+          <li className="notification-li">
+            <Link href="/">
+              <a>알림이 도착했습니다.</a>
+            </Link>
+          </li>
+          <li className="notification-li">
+            <Link href="/">
+              <a>알림이 도착했습니다.</a>
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
