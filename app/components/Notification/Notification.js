25cc46c9a4705290666729fc76ec695830815161
JeonMinJae <ssi02014@naver.com>
2021-05-24 14:14:13 +0900

    Add(minjae/notification): 알림 모달창 이외에영역 클릭 시 알림 모달 창 offf #49

--- app/components/Notification/Notification.js
+++ app/components/Notification/Notification.js
@@ -1,11 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Link from "next/link";
 import { IoMdNotifications } from "react-icons/io";
 
 const Notification = ({ studentId }) => {
   const [onNotification, setOnNotification] = useState(false);
 
-  useEffect(() => {}, []);
+  const refEl = useRef(null);
+
+  useEffect(() => {
+    const menuClickEvent = (e) => {
+      if (refEl.current !== null && !refEl.current.contains(e.target))
+        setOnNotification(!onNotification);
+    };
+
+    if (onNotification) window.addEventListener("click", menuClickEvent);
+
+    return () => window.removeEventListener("click", menuClickEvent);
+  }, [onNotification]);
 
   const onClick = (e) => {
     e.preventDefault();
@@ -23,7 +34,7 @@ const Notification = ({ studentId }) => {
       />
 
       {onNotification ? (
-        <ul className="notification-ul">
+        <ul className="notification-ul" ref={refEl}>
           <li className="notification-li nowatch">
             <Link href="/">
               <a>알림이 도착했습니다.</a>
