5981c9588eddc4c905c711413cf1296d4c8786d2
JeonMinJae <ssi02014@naver.com>
2021-05-26 00:04:15 +0900

    fix(minjae/layout): main banner fix #61

--- app/components/HomePage/MainBanner.js
+++ app/components/HomePage/MainBanner.js
@@ -6,7 +6,7 @@ const MainBanner = () => {
 
     const introduceSection = document.querySelector("#home-introduce");
 
-    window.scrollTo({ top: introduceSection.offsetTop - 70 });
+    window.scrollTo({ top: introduceSection.offsetTop - 60 });
   };
 
   return (
@@ -18,9 +18,9 @@ const MainBanner = () => {
         <p className="show">by. Wooahan Agile</p>
       </div>
 
-      <a className="banner-start-btn" onClick={onScroll}>
+      <button className="banner-start-btn" onClick={onScroll}>
         START
-      </a>
+      </button>
 
       <div className="banner-img">
         <img
@@ -28,7 +28,7 @@ const MainBanner = () => {
           src="https://wooahan-agile.s3.ap-northeast-2.amazonaws.com/HomePage/main-banner.png"
           alt="배너 이미지"
         />
-        <a onClick={onScroll}>START</a>
+        <button onClick={onScroll}>START</button>
       </div>
     </section>
   );
