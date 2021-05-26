6c5d0d16f94d17e3c69690d602c90831a4cc15a5
JeonMinJae <ssi02014@naver.com>
2021-05-26 15:40:17 +0900

    fix(minjae/develop): image upload limit desc

--- app/components/Editor/EditorImageUpload.js
+++ app/components/Editor/EditorImageUpload.js
@@ -7,36 +7,39 @@ const EditorImageUpload = ({
   handleDelete,
 }) => {
   return (
-    <div className="image-upload-box">
-      <label htmlFor="image-upload" className="image-upload-label">
-        <input
-          id="image-upload"
-          type="file"
-          multiple
-          accept="image/jpg,image/png,image/jpeg,image/gif"
-          onChange={handleImageUpload}
-          style={{ display: "none" }}
-        />
-        이미지 업로드 CLICK
-      </label>
+    <>
+      <div className="image-upload-box">
+        <label htmlFor="image-upload" className="image-upload-label">
+          <input
+            id="image-upload"
+            type="file"
+            multiple
+            accept="image/jpg,image/png,image/jpeg,image/gif"
+            onChange={handleImageUpload}
+            style={{ display: "none" }}
+          />
+          이미지 업로드 Click
+        </label>
 
-      <div className="image-preview-box">
-        {uploadImages &&
-          uploadImages.map((el, index) => {
-            return (
-              <div key={index} className="image-preview">
-                <img src={`${el}`} alt="미리보기" />
-                <div
-                  className="delete-image-btn"
-                  onClick={() => handleDelete(index)}
-                >
-                  <RiDeleteBin6Line />
+        <div className="image-preview-box">
+          {uploadImages &&
+            uploadImages.map((el, index) => {
+              return (
+                <div key={index} className="image-preview">
+                  <img src={`${el}`} alt="미리보기" />
+                  <div
+                    className="delete-image-btn"
+                    onClick={() => handleDelete(index)}
+                  >
+                    <RiDeleteBin6Line />
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+        </div>
       </div>
-    </div>
+      <p className="limit-desc">이미지 사용 제한: 4MB 이상, GIF 파일</p>
+    </>
   );
 };
 
