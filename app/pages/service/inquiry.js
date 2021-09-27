a8743e121423586aafa4559476f8c6495b903de8
JeonMinJae <ssi02014@naver.com>
2021-09-27 23:17:44 +0900

    Add(minjae/refactor): 문의사항 페이지 헤드 추가 #113

--- app/pages/service/inquiry.js
+++ app/pages/service/inquiry.js
@@ -1,10 +1,14 @@
 import React from "react";
 import BoardBanner from "../../components/Board/BoardBanner";
 import Inquiry from "../../components/Service/Inquiry";
+import Head from "next/head";
 
 const InqueryReceivePage = () => {
   return (
     <>
+      <Head>
+        <title>IUAM | 문의 사항</title>
+      </Head>
       <BoardBanner title="Service" desc="inquiry" />
       <section className="service-inquiry" id="service-inquiry">
         <div className="container">
